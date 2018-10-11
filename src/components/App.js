bc79414ca35085d2817dab9f93f2d14a3f229d0a
Jesse McReady <jessemcready@gmail.com>
2018-10-11 14:50:01 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,55 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import Pigs from './Pigs'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          < Nav />
+  state = {
+    filteredHogs: [...hogs]
+  }
 
-      </div>
+  handleClick = (event, hogs) => {
+    const action = event.target.name
+    if(action === 'weight'){
+      const objKey = 'weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'
+      const newFilter = this.state.filteredHogs.sort( (a,b) => a[objKey] - b[objKey])
+      this.setState({ filteredHogs: newFilter })
+    } else if(action === 'removeFilter'){
+      this.setState({
+        filteredHogs: [...hogs]
+      })
+    } else if(action === 'name') {
+      const newFilter = this.state.filteredHogs.sort( (a,b) => {
+        if(a[action] > b[action]){
+          return 1
+        } else if(a[action] < b[action]) {
+          return -1
+        } else {
+          return 0
+        }
+      })
+      this.setState({filteredHogs: newFilter})
+    } else {
+      const newFilter = hogs.filter( hog => hog.greased)
+      this.setState({filteredHogs: newFilter})
+    }
+  }
+
+  render() {
+    return ( <div className="App">
+        <Nav / >
+        <button name="name" className="ui basic blue button" onClick={event => this.handleClick(event, hogs)}>Sort By Name</button>
+        <button name="weight" className="ui basic blue button" onClick={event => this.handleClick(event, hogs)}>Sort By Weight</button>
+        <button name="greased" className="ui basic blue button" onClick={event => this.handleClick(event, hogs)}>Greased Hogs Only</button>
+        <button name="removeFilter" className="ui basic blue button" onClick={event => this.handleClick(event, hogs)}>Remove Filters</button>
+        <br></br>
+        <br></br>
+        <br></br>
+        <Pigs hogs={this.state.filteredHogs} />
+      < /div >
     )
   }
 }
