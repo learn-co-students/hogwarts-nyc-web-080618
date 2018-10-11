940f75a659b2f4b50b3c643727426230191f3400
github username <ariellenramirez@gmail.com>
2018-10-11 15:17:51 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import HogCard from './HogCard'
+import Buttons from './Buttons'
+
 
 class App extends Component {
+  state={
+    hogData: hogs
+  }
+
+  handleSort = (dataArray)=>{
+    this.setState({
+      hogData: dataArray
+    },()=>{console.log(this.state)})
+
+
+  }
+
   render() {
     return (
-      <div className="App">
-          < Nav />
+      <div className="ui grid container">
+
+          <Nav />
+          <br></br>
+      {this.state.hogData.map((hog,idx)=>{
+            console.log(hog)
+            return <HogCard key={idx} hogData={hog}/>
+
+          })}
+          <Buttons handleSort={this.handleSort} hogData={this.state.hogData}/>
 
       </div>
     )
